52be2f8c67d7c4e2777cbf7fdef723743d92cab7
skahsay <skahsay@paga.com>
2022-12-20 11:37:07 +0300

    fix added

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
 
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
+  // if (trueOrFalse) {
+  //   const task = false;
+  //   const task2 = true;
+  // }
 
   return [task, task2];
 }
