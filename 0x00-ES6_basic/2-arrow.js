f6b8c965941719c0f0fa48a1ddb82ced95c34eb7
skahsay <skahsay@paga.com>
2022-12-21 10:57:08 +0300

    fragmentd code added

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,6 +2,6 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = (newNeighborhood) => self.sanFranciscoNeighborhoods.push(newNeighborhood),
+  this.addNeighborhood = (newNeighborhood) => self.sanFranciscoNeighborhoods.push(newNeighborhood);
   self.sanFranciscoNeighborhoods;
 }
