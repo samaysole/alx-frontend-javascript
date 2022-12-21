0a1dd6db40eb0935b2b91f9df3ab37b8354bfbee
skahsay <skahsay@paga.com>
2022-12-21 15:42:26 +0300

    fix applied

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,6 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = (newNeighborhood) => self.sanFranciscoNeighborhoods.push(newNeighborhood);
-  self.sanFranciscoNeighborhoods;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
