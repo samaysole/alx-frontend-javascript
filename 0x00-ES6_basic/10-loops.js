cdfe4dfd44361ffc7e613e0085196d5815a93a85
skahsay <skahsay@paga.com>
2022-12-23 23:29:52 +0300

    tasks added

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx in array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+  for (const value of array) {
+    array[array.indexOf(value)] = appendString + value;
   }
 
   return array;
