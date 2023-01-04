7e4ebec78571f889fbc917bd9941b431c6c5b2cb
skahsay <skahsay@paga.com>
2023-01-04 23:53:38 +0300

    tasks added

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+    firstName: 'Maarten',
+    lastName: 'Okoth',
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {age: 23, ...row};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
